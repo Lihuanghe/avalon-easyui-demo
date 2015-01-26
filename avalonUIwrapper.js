78444430fd2e5a1d5bef1e83ad0f398945e78371
lihuanghe <huanghe.li@qunar.com>
2015-01-26 18:09:25 +0800

    add jQueryUI wrapper

--- avalonUIwrapper.js
+++ avalonUIwrapper.js
@@ -1,19 +1,19 @@
-﻿define('avalonUIwrapper',['avalon','jquery','easyui'],function(){
+﻿define('avalonUIwrapper',['avalon'],function(){
 var wrapper = function(componentName,init){
 	 var widget = avalon.ui[componentName] = function(element, data, vmodels) {
 		 var options = data[componentName+'Options'];
 		 var vmodel  = avalon.define(data[componentName+'Id'],function(vm){
 		            vm.wrapperOptions={};
-					vm.wrapper={};
+					vm.element=element;
 			   		avalon.mix(vm, options);
-	                vm.$init = function(callback) {//初始化组件的界面，最好定义此方法，让框架对它进行自动化配置
-	                	//调用使用wrapper来引用dom对象
-						vm.wrapper = $(element);
+					//初始化组件的界面，最好定义此方法，让框架对它进行自动化配置
+	                vm.$init = function(callback) {
 						//调用初始化方法
-						init(vm);
+						init(vm.$model);
 						callback();
                    }
-                   vm.$remove = function() {//清空构成UI的所有节点，最好定义此方法，让框架对它进行自动化销毁
+				   //清空构成UI的所有节点，最好定义此方法，让框架对它进行自动化销毁
+                   vm.$remove = function() {
                         element.innerHTML =  ""
                    }
 		   });
